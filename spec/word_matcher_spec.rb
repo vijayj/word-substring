ad923892f3d9e81f30d08ee4816249a2d33235db
Vijay J <vijayj80@yahoo.com>
2013-02-23 01:45:02 -0800

    some thoughts and a benchmark test

--- spec/word_matcher_spec.rb
+++ spec/word_matcher_spec.rb
@@ -9,65 +9,77 @@ describe "WordMatcherSpec" do
       w.longest_match.should be_nil
     end
   end
-  
+
   context "all words are equal, no matches" do
     let(:list){ %w(bat cat dog) }
     let(:list2) { %w(aal aar) }
-    
+
     it "should return nothing" do
       w = WordMatcher.new(list).find
       w.longest_match.should be_nil
     end
   end
-  
+
   context "word match but no subwords" do
     # let(:list){ %w( cat cats catsdogcats catxdogcatsrat dog dogcatsdog hippopotamuses rat ratcatdogcat )     }
     let(:list){ %w( cat cats catsdogcats catxdogcatsrat dog rat ratcatdogcat )     }
-    
+
     it "should return ratdogcat" do
       w = WordMatcher.new(list).find
-      w.longest_match.should == "ratcatdogcat"      
+      w.longest_match.should == "ratcatdogcat"
     end
   end
-  
+
   context "word with one character match" do
     let(:list) { %w(a aa aar b bapd c d d dex  r) }
-    
-    
+
+
     it "should return aar" do
       w = WordMatcher.new(list).find
-      w.longest_match.should == "aar"      
+      w.longest_match.should == "aar"
     end
   end
-  
+
   context "word match with many subwords match" do
-    let(:list){ %w(bat cat cats dog rat ratdogcat ratcatsdog t ttttt) }    
+    let(:list){ %w(bat cat cats dog rat ratdogcat ratcatsdog t ttttt) }
 
-    it "should return ratcatsdog" do      
+    it "should return ratcatsdog" do
       w = WordMatcher.new(list).find
-      w.longest_match.should == "ratcatsdog"          
+      w.longest_match.should == "ratcatsdog"
     end
   end
-  
-  
-  context "using a file of words" do
-      before(:each) do
-        @list = []
-        File.open(File.join(File.dirname(__FILE__), "fixtures", "words_for_problem.txt")).each_line do |l|
-          @list << l.chomp
-        end
-      end
 
-      it "should return longest word" do      
-        w = WordMatcher.new(@list).find
-        w.longest_match.should == "ethylenediaminetetraacetates"          
-      end  
+
+  context "using a file of words" do
+    before(:each) do
+      @list = []
+      File.open(File.join(File.dirname(__FILE__), "fixtures", "words_for_problem.txt")).each_line do |l|
+        @list << l.chomp
+      end
+    end
+
+    it "should return longest word" do
+      w = WordMatcher.new(@list).find
+      w.longest_match.should == "ethylenediaminetetraacetates"
+    end
+    
+    it "should return longest word benchmarked " do
+      pending('soon')
+      n = 100
+      require 'benchmark'
+      Benchmark.bm do |x|
+        w = nil
+        x.report { w = WordMatcher.new(@list).find }
+        w.longest_match.should == "ethylenediaminetetraacetates"
+      end
+    end
   end
-  
+
   context "count all matched words" do
     it "should return a count" do
       pending
     end
   end
-  
+
 end
+
